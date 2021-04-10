226727d1ac4103fd46db221fb52c19cb4ccf3d06
taekkim <taekkim@student.42seoul.kr>
2021-04-11 07:45:28 +0900

    FEAT : lesson2.py

---
+++ python/lesson2.py
@@ -0,0 +1,20 @@
+"""
+1.
+
+Complete the body of the following function according to its docstring.
+
+HINT: Python has a built-in function round.
+"""
+
+
+def round_to_two_places(num):
+    """Return the given number rounded to two decimal places.
+
+    >>> round_to_two_places(3.14159)
+    3.14
+    """
+    # Replace this body with your own code.
+    # ("pass" is a keyword that does literally nothing. We used it as a placeholder
+    # because after we begin a code block, Python requires at least one line of code)
+    return (round(num, 2))
+
